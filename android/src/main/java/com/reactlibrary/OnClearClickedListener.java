2f1e0d0459dfb8e1a4be6c41c73a5884a53f730d
Varian <varianpoan@gmail.com>
2019-10-03 14:00:36 +0700

    Initial Commit

---
+++ android/src/main/java/com/reactlibrary/OnClearClickedListener.java
@@ -0,0 +1,7 @@
+package com.reactlibrary;
+
+import android.content.DialogInterface;
+
+public interface OnClearClickedListener {
+    void onClearClicked(DialogInterface dialogInterface);
+}
---
+++ ios/CustomDatePicker.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface CustomDatePicker : NSObject <RCTBridgeModule>
+
+@end
